0a5a0b6283d10a56c0790b44e9d6c7a20fbb13c6
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2019-06-10 15:35:35 +0200

    Created empty module for WhatsApp notification gateway

---
+++ VirtoCommerce.WhatsAppNotification.Web/Module.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using VirtoCommerce.Platform.Core.Modularity;
+
+namespace VirtoCommerce.WhatsAppNotification.Web
+{
+    public class Module : ModuleBase
+    {
+        private readonly IUnityContainer _container;
+
+        public Module(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
